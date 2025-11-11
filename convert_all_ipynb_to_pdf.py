88fd0aea5feb168cd7fc4bc1a9a3dbc3f8d5075d
LuminescentNebula <dima.lippo@bk.ru>
2025-11-12 02:24:30 +0300

    Обновление скрипты для сборки

--- convert_all_ipynb_to_pdf.py
+++ convert_all_ipynb_to_pdf.py
@@ -1,7 +1,9 @@
 import subprocess
 import os
-from pathlib import Path
+from pathlib import Path,PosixPath
 from pprint import pprint
+import sys
+from playwright.sync_api import sync_playwright
 
 root_path = Path('.')
 
@@ -14,7 +16,8 @@ jn_command = [
         "--allow-chromium-download",
         "--output-dir", 
         None,
-        "--log-level=WARN"
+        "--log-level=WARN",
+        "--WebPDFExporter.paginate=False"
     ]
 
 def all_convert_ipynb_to_pdf():
@@ -46,19 +49,75 @@ def convert_ipynb_to_pdf(ipynb_path: Path):
     print(f'Converted "{ipynb_path}" to "{pdf_path}"')
 
 
-def all_convert_py_to_txt():
+def all_change_extension():
     files = [p for p in root_path.rglob('*.py') if 'Теория' in p.parts]
     pprint(files)
-    for nb in files:
-        convert_py_to_txt(nb)    
+    for file in files:
+        change_extension(file, "txt")    
 
-def convert_py_to_txt(path: Path):
+def change_extension(path: Path, to:str):
     new_path_parts = ('Релиз',) + path.parts
     new_path = Path(*new_path_parts)
     new_path.parent.mkdir(parents=True, exist_ok=True)
-    new_path = new_path.with_suffix(".txt")
+    new_path = new_path.with_suffix("."+ to)
     print('"'+str(path)+'"', '".\\'+str(new_path)+'"')
-    os.system(" ".join(["copy",'"'+str(path)+'"', '"'+str(new_path)+'"']))
+    if isinstance(new_path, PosixPath):
+        os.system(" ".join(["cp",'"'+str(path)+'"', '"'+str(new_path)+'"']))
+    else:
+        os.system(" ".join(["copy",'"'+str(path)+'"', '"'+str(new_path)+'"']))
 
-#all_convert_ipynb_to_pdf()
-all_convert_py_to_txt()
+def all_convert_drawio():
+    files = [p for p in root_path.rglob('*.drawio') if 'Теория' in p.parts]
+    pprint(files)
+    for file in files:
+        convert_drawio(file)    
+
+def convert_drawio(path: Path):
+    pass
+
+def all_html():
+    files = [p for p in root_path.rglob('*.html') if ('Теория' in p.parts) and ('Релиз' not in p.parts)]
+    pprint(files)
+    for file in files:
+        copy_html(file)  
+        convert_html_to_pdf(file)
+
+def copy_html(path: Path):
+    new_path_parts = ('Релиз',) + path.parts
+    new_path = Path(*new_path_parts)
+    new_path.parent.mkdir(parents=True, exist_ok=True)
+    print('"'+str(path)+'"', '"'+str(new_path)+'"')
+    if isinstance(new_path, PosixPath):
+        os.system(" ".join(["cp",'"'+str(path)+'"', '"'+str(new_path)+'"']))
+    else:
+        os.system(" ".join(["copy",'"'+str(path)+'"', '"'+str(new_path)+'"']))
+
+def convert_html_to_pdf(path: Path):
+    new_path_parts = ('Релиз',) + path.parts
+    new_path = Path(*new_path_parts)
+    new_path.parent.mkdir(parents=True, exist_ok=True)
+    new_path = new_path.with_suffix(".pdf")
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(f"file://{path.absolute()}", wait_until="networkidle")
+        # Получаем высоту содержимого страницы
+        height = page.evaluate("() => document.documentElement.scrollHeight")
+        
+        # Генерируем PDF с высотой всей страницы (без разрывов)
+        page.pdf(
+            path=".\\"+str(new_path),
+            width="210mm",               # можно задать нужную ширину
+            height=f"{height}px",
+            print_background=True,
+            margin={"top": "0px", "bottom": "0px", "left": "0px", "right": "0px"}
+        )
+        browser.close()
+    print("Converted to", new_path)
+
+
+all_convert_ipynb_to_pdf()
+all_change_extension()
+#all_convert_drawio()
+all_html()
